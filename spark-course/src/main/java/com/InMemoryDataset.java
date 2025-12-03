6a46871b990de723e63bc8935d3453275673d12e
murilolocatelli <murilolocatelli@gmail.com>
2025-12-03 08:47:35 -0300

    .

---
+++ spark-course/src/main/java/com/InMemoryDataset.java
@@ -0,0 +1,56 @@
+package com;
+
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.apache.spark.sql.functions.col;
+
+public class InMemoryDataset {
+    public static void main(String[] args) {
+        //Logger.getLogger("org.apache").setLevel(Level.WARN);
+        //Logger.getLogger("org.sparkproject").setLevel(Level.WARN);
+
+        System.setProperty("hadoop.home.dir", "c:/hadoop");
+
+        SparkSession sparkSession = SparkSession.builder()
+                .appName("Testing SQL")
+                .master("local[*]")
+                .getOrCreate();
+
+        List<Row> inMemory = new ArrayList<>();
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL","2015-4-21 19:23:20"));
+
+        StructField[] structFields = new StructField[] {
+                new StructField("level", DataTypes.StringType,false, Metadata.empty()),
+                new StructField("datetime",DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType structType = new StructType(structFields);
+
+        Dataset<Row> df = sparkSession.createDataFrame(inMemory, structType);
+
+        df.createOrReplaceTempView("logging_table");
+
+        sparkSession.sql("select level, count(datetime) from logging_table group by level").show();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        sparkSession.close();
+    }
+}
--- spark-course/src/main/java/com/MainForSQL.java
+++ spark-course/src/main/java/com/MainForSQL.java
@@ -1,8 +1,15 @@
 package com;
 
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
+
+import java.util.Scanner;
+
+import static org.apache.spark.sql.functions.col;
 
 public class MainForSQL {
     public static void main(String[] args) {
@@ -17,10 +24,28 @@ public class MainForSQL {
                 .getOrCreate();
 
         Dataset<Row> dataset = sparkSession.read().option("header", true).csv("src/main/resources/exams/students.csv");
-        dataset.show();
 
-        long count = dataset.count();
-        System.out.println("Number of rows: " + count);
+        FilterFunction<Row> func = row -> row.getAs("aaa").equals("Modern Art");
+
+        //Dataset<Row> modernArtResults = dataset.filter("subject = 'Modern Art' and year >= 2007");
+        //Dataset<Row> modernArtResults = dataset.filter((FilterFunction<Row>) row ->
+        //        row.getAs("subject").equals("Modern Art") && Integer.parseInt(row.getAs("year")) >= 2007);
+
+        Dataset<Row> modernArtResults = dataset
+                .filter(col("subject").equalTo("Modern Art")
+                        .and(col("year").geq (2007)));
+
+        modernArtResults.show();
+
+        dataset.createOrReplaceTempView("my_students_view");
+
+        //Dataset<Row> viewResults = sparkSession.sql("select distinct(year) from my_students_view");
+        Dataset<Row> viewResults = sparkSession.sql("select avg(score), max(score), min(score) from my_students_view where subject = 'French' and year >= 2007");
+
+        viewResults.show();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
 
         sparkSession.close();
     }
